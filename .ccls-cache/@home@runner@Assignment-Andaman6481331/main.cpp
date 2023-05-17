5f1fd1193be53e0fbd3a5ec40a9329c4d6e77bea
Andaman6481331 <132637308+Andaman6481331@users.noreply.github.com>
2023-05-17 09:13:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Assignment-Andaman6481331/main.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include <iostream>
+#include <string.h>
+using namespace std;
+#define SIZE 100
+
+int main(int argc, char **argv){
+  int i,n,*a;
+            n=argc-1;
+              a=new int[n];
+            for(i=0;i<n;i++) {
+                a[i]=atoi(argv[i+1]);
+                 cout<< "a[" << i << "]=" <<a[i]<<endl;
+        }
+
+  int*p=a;
+  int*pb=a+n-1;
+  int temp;
+  for (i = 0; i < n/ 2; i++) {
+
+    temp = *p;
+    *p = *pb;
+    *pb = temp;
+    p++;
+    pb--;
+  }
+  p=a;
+  cout<<"reverse"<<endl;
+  for(i=0;i<n;i++) {
+                 cout<< "a[" << i << "]=" <<a[i]<<endl;
+  }
+ delete [] p;  
+}
